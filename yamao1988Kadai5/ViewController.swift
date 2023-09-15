b44dea80157ca11e1e8b6944992ca5e98dcba483
Tsuyoshi Kamada <ms.2bpd.cch@gmail.com>
2023-09-16 01:14:56 +0900

    ⒈let num1.2のtextFieldの形を変更した。 ⒉条件式を一部変更 ⒊割る数に入った値が０以外の場合にアラートを表示する項目を追加 ⒋割られる数に入った値が０以外の場合にアラートを表示する項目を追加 ⒌最後の計算式で要素の形を変更した

--- yamao1988Kadai5/ViewController.swift
+++ yamao1988Kadai5/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
     }
     
     func calculate() {
-        let num1 = ((text1.text ?? "") as NSString).floatValue
-        let num2 = ((text2.text ?? "") as NSString).floatValue
+        let num1 = Float(text1.text ?? "")
+        let num2 = Float(text2.text ?? "")  //数値も文字列も０と見なしている。Num１とnum2文字列と０を区別できるように条件分岐できるようにする。by Seigetsuくんより
         
         if text1.text?.isEmpty ?? true {
             // アラート作成
@@ -27,8 +27,8 @@ class ViewController: UIViewController {
             
             // アラートを表示
             present(alert, animated: true, completion: nil)
-        }
-        if text2.text?.isEmpty ?? true {
+            
+        } else if text2.text?.isEmpty ?? true {
             // アラート作成
             let title = "課題５"
             let message = "割る数を入力して下さい"
@@ -37,11 +37,33 @@ class ViewController: UIViewController {
             // OKボタンを作成＆追加
             let ok = UIAlertAction(title: "OK", style: .default, handler:nil)
             alert.addAction(ok)
-            
+
             // アラートを表示
             present(alert, animated: true, completion: nil)
             
-        } else if num2 == 0 {
+        } else if num1 != (self.text1.text! as NSString).floatValue {
+            // アラート作成
+            let title = "課題５"
+            let message = "割られる数に数値以外を入力しないで下さい"
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            // OKボタンを作成＆追加
+            let ok = UIAlertAction(title: "OK", style: .default, handler:nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+            
+        } else if num2 != (self.text2.text! as NSString).floatValue {
+            // アラート作成
+            let title = "課題５"
+            let message = "割る数に数値以外を入力しないで下さい"
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            // OKボタンを作成＆追加
+            let ok = UIAlertAction(title: "OK", style: .default, handler:nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+            
+        } else if num2 == 0  {
             // アラート作成
             let title = "課題５"
             let message = "割る数に0を入力しないで下さい"
@@ -52,9 +74,20 @@ class ViewController: UIViewController {
             alert.addAction(ok)
             present(alert, animated: true, completion: nil)
             
-        } else if num2 != 0 {
+        } else if num2 != 0 && text2.text == nil {
+            // アラート作成
+            let title = "課題５"
+            let message = "割る数には実数を入力してください"
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            
+            // OKボタンを作成＆追加
+            let ok = UIAlertAction(title: "OK", style: .default, handler:nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+            
+        } else {
                 //処理
-                let result = num1 / num2
+            let result = (num1 ?? 0) / (num2 ?? 0)
                 //出力
                 label.text = "\(result)"
         }
